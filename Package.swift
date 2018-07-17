db0fe2673d769b6f2aa3273a35213e5b9d7fcc83
Alexandre Mantovani Tavares <alexandre@live.in>
2018-07-17 16:10:23 -0300

    fix: package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,28 @@
-//
-//  Nappa.swift
-//  Nappa
-//
-//  Created by Alexandre Mantovani tavares on 23/10/15.
-//  Copyright © 2017 Nappa. All rights reserved.
-//
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Nappa",
-    dependencies: [],
-    exclude: ["Tests"]
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Nappa",
+            targets: ["Nappa"]),
+        ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Nappa",
+            dependencies: []),
+        .testTarget(
+            name: "NappaTests",
+            dependencies: ["Nappa"]),
+        ]
 )
